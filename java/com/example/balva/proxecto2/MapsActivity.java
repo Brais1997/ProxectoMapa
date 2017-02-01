e24536d345bb6c243c465e38abf95e1c737e6943
Brais Alvares Escudero <Brais Alvares Escudero>
2017-02-01 13:53:16 +0100

    Circulo , marca e zoom creado

---
+++ java/com/example/balva/proxecto2/MapsActivity.java
@@ -0,0 +1,55 @@
+package com.example.balva.proxecto2;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LatLng centro = new LatLng(42.237023, -8.717944);
+    int radio = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng catedral = new LatLng(42.237023, -8.717944);
+        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral"));
+
+        CircleOptions circuloCaracteristicas = new CircleOptions()
+                .center(centro)
+                .radius(radio)
+                .strokeColor(Color.parseColor("#0D47A1"))
+                .strokeWidth(4)
+                .fillColor(Color.argb(32, 33, 150, 243));
+        mMap.addCircle(circuloCaracteristicas);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(catedral,17));
+
+        }
+
+    }
+
