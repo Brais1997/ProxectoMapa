ce2ed5b14fe27a897fc3ccda1005623f99cd3777
Brais Alvares Escudero <Brais Alvares Escudero>
2017-02-02 13:39:09 +0100

    Añadido circulo de marca e que me mostre a miña ubicación (sen recoller coordenadas)

--- java/com/example/balva/proxecto2/MapsActivity.java
+++ java/com/example/balva/proxecto2/MapsActivity.java
@@ -2,10 +2,12 @@ package com.example.balva.proxecto2;
 
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,10 +19,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    private static final int LOCATION_REQUEST_CODE=1;
+    private static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     LatLng centro = new LatLng(42.237023, -8.717944);
+    LatLng cmarca = new LatLng(42.237558, -8.717285);
     int radio = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,22 +36,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng catedral = new LatLng(42.237023, -8.717944);
-        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral"));
-
+        CircleOptions marca = new CircleOptions()
+                .center(cmarca)
+                .radius(20)
+                .strokeColor(Color.parseColor("#FF4000"))
+                .strokeWidth(4)
+                .fillColor(Color.argb(32, 33, 150, 243));
+        LatLng premio = new LatLng(42.237558, -8.717285);
+        mMap.addMarker(new MarkerOptions().position(premio).title("Premio"));
+        mMap.addCircle(marca).setVisible(true);
         CircleOptions circuloCaracteristicas = new CircleOptions()
                 .center(centro)
                 .radius(radio)
                 .strokeColor(Color.parseColor("#0D47A1"))
                 .strokeWidth(4)
                 .fillColor(Color.argb(32, 33, 150, 243));
-        mMap.addCircle(circuloCaracteristicas);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(catedral,17));
+
+        mMap.addCircle(circuloCaracteristicas).setVisible(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(premio, 17));
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -64,9 +73,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         LOCATION_REQUEST_CODE);
             }
         }
-
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            // ¿Permisos asignados?
+            if (permissions.length > 0 &&
+                    permissions[0].equals(android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Error de permisos", Toast.LENGTH_LONG).show();
+            }
+
         }
+    }
 
     }
 
